445a1b8f81183975ac7efb88e26fc543aec28de1
Chris Moeller <kode54@gmail.com>
2010-01-11 09:00:02 +0000

    {5/31/2006 8:38:50 PM}2006-05-31 19:09 UTC - kode54 - Implemented fading support.
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection/]$/foobar2000/files/plugins.root;C97

--- resource.h
+++ resource.h
@@ -3,29 +3,27 @@
 // Used by mod.rc
 //
 #define IDD_MOD_CONFIG                  101
-#define IDD_CONTEXT                     102
 #define IDC_SAMPLERATE                  1000
-#define IDC_ORDER                       1000
 #define IDC_INTERPOLATION               1001
-#define IDC_REPLACE                     1001
-#define IDC_LOOPS                       1002
-#define IDC_RANGE                       1002
-#define IDC_TAG                         1003
-#define IDC_TRIM                        1004
-#define IDC_SCAN                        1005
+#define IDC_VOLRAMP                     1002
+#define IDC_LOOPS                       1003
+#define IDC_TAG                         1004
+#define IDC_TRIM                        1005
 #define IDC_DYNAMIC_INFO                1006
-#define IDC_VOLRAMP                     1007
-#define IDC_CHIP_BOX                    1008
-#define IDC_CHIP                        1009
-#define IDC_CHIP_FORCE                  1010
-#define IDC_CHIP_SCAN                   1011
-#define IDC_CHIP_SCAN_THRESHOLD         1012
-#define IDC_CHIP_TEXT_0                 1013
-#define IDC_CHIP_TEXT_1                 1014
-#define IDC_CHIP_TEXT_2                 1015
-#define IDC_CHIP_TEXT_3                 1016
-#define IDC_CHIP_TEXT_4                 1017
-#define IDC_CHIP_TEXT_5                 1018
+#define IDC_FADE                        1007
+#define IDC_FADE_TIME                   1008
+#define IDC_FADE_TEXT                   1009
+#define IDC_CHIP_BOX                    1010
+#define IDC_CHIP                        1011
+#define IDC_CHIP_FORCE                  1012
+#define IDC_CHIP_SCAN                   1013
+#define IDC_CHIP_SCAN_THRESHOLD         1014
+#define IDC_CHIP_TEXT_0                 1015
+#define IDC_CHIP_TEXT_1                 1016
+#define IDC_CHIP_TEXT_2                 1017
+#define IDC_CHIP_TEXT_3                 1018
+#define IDC_CHIP_TEXT_4                 1019
+#define IDC_CHIP_TEXT_5                 1020
 
 // Next default values for new objects
 // 
@@ -33,7 +31,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
